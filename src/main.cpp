5145f9df6833d7c59605ca70e1d6ff284aae64b7
Dipod <dipod.dopid@gmail.com>
2020-10-14 16:26:17 +0300

    Task "All in square" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,59 @@
 #include <map>
-#include <string>
 #include <iostream>
-#include <stdexcept>
+#include <vector>
+#include <tuple>
 
 using namespace std;
-template <typename key, typename value>
-value& GetRefStrict(map<key, value> &m, const key &k) {
-	if (m.count(k) == 0) {
-		throw runtime_error("Key not exists");
+
+template<typename BasicType> BasicType Sqr(const BasicType &item);
+template<typename First, typename Second> pair<First, Second> Sqr(
+		const pair<First, Second> p1);
+template<typename Element> vector<Element> Sqr(vector<Element> c);
+template<typename Key, typename Value> map<Key, Value> Sqr(map<Key, Value> c);
+
+template<typename BasicType>
+BasicType Sqr(const BasicType &item) {
+	return item * item;
+}
+
+template<typename First, typename Second>
+pair<First, Second> Sqr(const pair<First, Second> p1) {
+	First f = Sqr(p1.first);
+	Second s = Sqr(p1.second);
+	return {f, s};
+}
+
+template<typename Element>
+vector<Element> Sqr(vector<Element> c) {
+	for (Element &item : c) {
+		item = Sqr(item);
 	}
-	return m.at(k);
+	return c;
+}
+
+template<typename Key, typename Value>
+map<Key, Value> Sqr(map<Key, Value> c) {
+	for (auto &item : c) {
+		item.second = Sqr(item.second);
+	}
+	return c;
 }
 
 int main() {
 
-	map<int, string> m = { { 0, "value" } };
-	string &item = GetRefStrict(m, 0);
-	item = "newvalue";
-	cout << m[0] << endl; // выведет newvalue
+	vector<int> v = { 1, 2, 3 };
+	cout << "vector:";
+	for (int x : Sqr(v)) {
+		cout << ' ' << x;
+	}
+	cout << endl;
+
+	map<int, pair<int, int>> map_of_pairs = { { 4, { 2, 2 } }, { 7, { 4, 3 } } };
+	cout << "map of pairs:" << endl;
+	for (const auto &x : Sqr(map_of_pairs)) {
+		cout << x.first << ' ' << x.second.first << ' ' << x.second.second
+				<< endl;
+	}
 
 	return 0;
 }
