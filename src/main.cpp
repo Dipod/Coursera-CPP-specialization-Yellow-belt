1fafd4212bcc0596446d6b3f98ef7b63a53ffa54
Dipod <dipod.dopid@gmail.com>
2020-10-18 12:17:05 +0300

    Task "Write class declaration" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,19 @@
 #include "test_runner.h"
-#include <vector>
-#include "phone_number.h"
-#include <stdexcept>
+#include "rectangle.h"
 
 using namespace std;
 
 // Tests section
 
-void TestPhoneNumber() {
+void TestSomething() {
 
-	{
-		PhoneNumber p("+7-495-111-22-33");
-		AssertEqual(p.GetCountryCode(), "7",
-				"Country code of number +7-495-111-22-33");
-		AssertEqual(p.GetCityCode(), "495",
-				"City code of number +7-495-111-22-33");
-		AssertEqual(p.GetLocalNumber(), "111-22-33",
-				"Local number of number +7-495-111-22-33");
-		AssertEqual(p.GetInternationalNumber(), "+7-495-111-22-33",
-				"International number of number +7-495-111-22-33");
-	}
-
-	{
-		PhoneNumber p("+7-495-1112233");
-		AssertEqual(p.GetCountryCode(), "7",
-				"Country code of number +7-495-1112233");
-		AssertEqual(p.GetCityCode(), "495",
-				"City code of number +7-495-1112233");
-		AssertEqual(p.GetLocalNumber(), "1112233",
-				"Local number of number +7-495-1112233");
-		AssertEqual(p.GetInternationalNumber(), "+7-495-1112233",
-				"International number of number +7-495-1112233");
-	}
-
-	{
-		PhoneNumber p("+323-22-460002");
-		AssertEqual(p.GetCountryCode(), "323",
-				"Country code of number +323-22-460002");
-		AssertEqual(p.GetCityCode(), "22",
-				"City code of number +323-22-460002");
-		AssertEqual(p.GetLocalNumber(), "460002",
-				"Local number of number +323-22-460002");
-		AssertEqual(p.GetInternationalNumber(), "+323-22-460002",
-				"International number of number +323-22-460002");
-	}
-
-	{
-		PhoneNumber p("+1-2-coursera-cpp");
-		AssertEqual(p.GetCountryCode(), "1",
-				"Country code of number +1-2-coursera-cpp");
-		AssertEqual(p.GetCityCode(), "2",
-				"City code of number +1-2-coursera-cpp");
-		AssertEqual(p.GetLocalNumber(), "coursera-cpp",
-				"Local number of number +1-2-coursera-cpp");
-		AssertEqual(p.GetInternationalNumber(), "+1-2-coursera-cpp",
-				"International number of number +1-2-coursera-cpp");
-	}
-
-	{
-		try {
-			PhoneNumber p("1-2-333");
-		} catch (invalid_argument &ex) {
-		}
-	}
-
-	{
-		try {
-			PhoneNumber p("+7-1233");
-		} catch (invalid_argument &ex) {
-		}
-	}
 }
 
 // End of tests section
 
 void TestAll() {
 	TestRunner tr;
-	tr.RunTest(TestPhoneNumber, "TestPhoneNumber");
+	tr.RunTest(TestSomething, "TestSomething");
 }
 
 int main() {
