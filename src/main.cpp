9a9dffadb624704d751c8a607247296c5dcb3a89
Dipod <dipod.dopid@gmail.com>
2020-10-19 20:30:08 +0300

    Task "Set part" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,53 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <set>
+#include <string>
 #include "test_runner.h"
 
 using namespace std;
 
-vector<int> GetVectorPart(const vector<int> &numbers) {
-	auto it = find_if(numbers.begin(), numbers.end(), [](const int &number) {
-		return number < 0;
-	});
-	vector<int> result;
-	result.assign(numbers.begin(), it);
-	reverse(result.begin(), result.end());
-	return result;
-}
+template<typename T>
+vector<T> FindGreaterElements(const set<T> &elements, const T &border) {
 
-void PrintVectorPart(const vector<int> &numbers) {
-	bool first = true;
-	for (const auto &item : GetVectorPart(numbers)) {
-		if (!first) {
-			cout << ' ';
-		}
-		cout << item;
-		first = false;
-	}
+	auto it = find_if(begin(elements), end(elements), [&border](const T &item) {
+		return item > border;
+	});
+
+	vector<T> result;
+	result.assign(it, end(elements));
+	return result;
 }
 
 // Tests section
 
-void TestGetVectorPart() {
-	{
-		vector<int> n = { 8, 1, 6 };
-		AssertEqual(GetVectorPart( { 6, 1, 8, -5, 4 }), n,
-				"Test vector { 6, 1, 8, -5, 4 }");
-	}
+void TestFindGreaterElements() {
 
-	{
-		vector<int> n;
-		AssertEqual(GetVectorPart( { -6, 1, 8, -5, 4 }), n,
-				"Test vector { -6, 1, 8, -5, 4 }");
-	}
+	AssertEqual(FindGreaterElements(set<int> { 1, 5, 7, 8 }, 5), vector<int> {
+			7, 8 }, "Test set of { 1, 5, 7, 8 }");
+	string to_find = "Python";
+	AssertEqual(FindGreaterElements(set<string> { "C", "C++" }, to_find),
+			vector<string> { }, "Test set of { C, C++ }");
 
-	{
-		vector<int> n = { 4, 5, 8, 1, 6 };
-		AssertEqual(GetVectorPart( { 6, 1, 8, 5, 4 }), n,
-				"Test vector { 6, 1, 8, 5, 4 }");
-	}
 }
 
 // End of tests section
 
 void TestAll() {
 	TestRunner tr;
-	tr.RunTest(TestGetVectorPart, "Test TestGetVectorPart");
+	tr.RunTest(TestFindGreaterElements, "Test FindGreaterElements");
 }
 
 int main() {
 	TestAll();
 
-	PrintVectorPart( { 6, 1, 8, -5, 4 });
-	cout << endl;
-	PrintVectorPart( { -6, 1, 8, -5, 4 });
-	cout << endl;
-	PrintVectorPart( { 6, 1, 8, 5, 4 });
+	for (int x : FindGreaterElements(set<int> { 1, 5, 7, 8 }, 5)) {
+		cout << x << " ";
+	}
 	cout << endl;
 
+	string to_find = "Python";
+	cout << FindGreaterElements(set<string> { "C", "C++" }, to_find).size()
+			<< endl;
 	return 0;
 }
