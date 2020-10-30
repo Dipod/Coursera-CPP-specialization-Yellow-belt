584ef8272e44b5f8adf1ba737695a30cabbf96c6
Dipod <dipod.dopid@gmail.com>
2020-10-30 21:55:53 +0300

    Task "Demographic indicators" solution

--- src/main.cpp
+++ src/main.cpp
@@ -5,49 +5,64 @@
 
 using namespace std;
 
-template<typename T>
-void RemoveDuplicates(vector<T> &elements) {
-	set<T> s(begin(elements), end(elements));
-	elements.assign(begin(s), end(s));
-}
+enum class Gender {
+	FEMALE, MALE
+};
 
-// Tests section
+struct Person {
+	int age;  // возраст
+	Gender gender;  // пол
+	bool is_employed;  // имеет ли работу
+};
 
-void TestSomething() {
-
-}
-
-// End of tests section
-
-void TestAll() {
-	TestRunner tr;
-	tr.RunTest(TestSomething, "Test TestSomething");
-}
-
-ostream& operator<<(ostream &output, const vector<int> &v) {
-	for (auto it = begin(v); it < end(v); it++) {
-		if (it != begin(v)) {
-			output << ' ';
-		}
-		output << *it;
+// Это пример функции, его не нужно отправлять вместе с функцией PrintStats
+template<typename InputIt>
+int ComputeMedianAge(InputIt range_begin, InputIt range_end) {
+	if (range_begin == range_end) {
+		return 0;
 	}
-	return output;
+	vector<typename InputIt::value_type> range_copy(range_begin, range_end);
+	auto middle = begin(range_copy) + range_copy.size() / 2;
+	nth_element(begin(range_copy), middle, end(range_copy),
+			[](const Person &lhs, const Person &rhs) {
+				return lhs.age < rhs.age;
+			}
+	);
+	return middle->age;
+}
+
+void PrintStats(vector<Person> persons) {
+	cout << "Median age = " << ComputeMedianAge(persons.begin(), persons.end())
+			<< endl;
+	auto it = partition(persons.begin(), persons.end(), [](Person &i) {
+		return i.gender == Gender::FEMALE;
+	});
+	cout << "Median age for females = " << ComputeMedianAge(persons.begin(), it)
+			<< endl;
+	cout << "Median age for males = " << ComputeMedianAge(it, persons.end())
+			<< endl;
+	auto it2 = partition(persons.begin(), it, [](Person &i) {
+		return i.is_employed;
+	});
+	cout << "Median age for employed females = "
+			<< ComputeMedianAge(persons.begin(), it2) << endl;
+	cout << "Median age for unemployed females = " << ComputeMedianAge(it2, it)
+			<< endl;
+	it2 = partition(it, persons.end(), [](Person &i) {
+		return i.is_employed;
+	});
+	cout << "Median age for employed males = " << ComputeMedianAge(it, it2)
+			<< endl;
+	cout << "Median age for unemployed males = "
+			<< ComputeMedianAge(it2, persons.end()) << endl;
 }
 
 int main() {
-
-	int n;
-	cin >> n;
-	vector<int> permutation(n);
-	for (auto &item : permutation) {
-		item = n--;
-	}
-
-	cout << permutation << endl;
-
-	while (prev_permutation(begin(permutation), end(permutation))) {
-		cout << permutation << endl;
-	}
-
+	vector<Person> persons = { { 31, Gender::MALE, false }, { 40,
+			Gender::FEMALE, true }, { 24, Gender::MALE, true }, { 20,
+			Gender::FEMALE, true }, { 80, Gender::FEMALE, false }, { 78,
+			Gender::MALE, false }, { 10, Gender::FEMALE, false }, { 55,
+			Gender::MALE, true }, };
+	PrintStats(persons);
 	return 0;
 }
