56d2bd8b5c731ef595929da8fc9923d3f7014eed
Dipod <dipod.dopid@gmail.com>
2020-10-31 11:35:36 +0300

    Task "Merge sort" part 1 solution

--- src/main.cpp
+++ src/main.cpp
@@ -5,64 +5,28 @@
 
 using namespace std;
 
-enum class Gender {
-	FEMALE, MALE
-};
+template<typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end) {
 
-struct Person {
-	int age;  // возраст
-	Gender gender;  // пол
-	bool is_employed;  // имеет ли работу
-};
+	auto range_length = range_end - range_begin;
 
-// Это пример функции, его не нужно отправлять вместе с функцией PrintStats
-template<typename InputIt>
-int ComputeMedianAge(InputIt range_begin, InputIt range_end) {
-	if (range_begin == range_end) {
-		return 0;
+	if (range_length < 2) {
+		return;
 	}
-	vector<typename InputIt::value_type> range_copy(range_begin, range_end);
-	auto middle = begin(range_copy) + range_copy.size() / 2;
-	nth_element(begin(range_copy), middle, end(range_copy),
-			[](const Person &lhs, const Person &rhs) {
-				return lhs.age < rhs.age;
-			}
-	);
-	return middle->age;
-}
 
-void PrintStats(vector<Person> persons) {
-	cout << "Median age = " << ComputeMedianAge(persons.begin(), persons.end())
-			<< endl;
-	auto it = partition(persons.begin(), persons.end(), [](Person &i) {
-		return i.gender == Gender::FEMALE;
-	});
-	cout << "Median age for females = " << ComputeMedianAge(persons.begin(), it)
-			<< endl;
-	cout << "Median age for males = " << ComputeMedianAge(it, persons.end())
-			<< endl;
-	auto it2 = partition(persons.begin(), it, [](Person &i) {
-		return i.is_employed;
-	});
-	cout << "Median age for employed females = "
-			<< ComputeMedianAge(persons.begin(), it2) << endl;
-	cout << "Median age for unemployed females = " << ComputeMedianAge(it2, it)
-			<< endl;
-	it2 = partition(it, persons.end(), [](Person &i) {
-		return i.is_employed;
-	});
-	cout << "Median age for employed males = " << ComputeMedianAge(it, it2)
-			<< endl;
-	cout << "Median age for unemployed males = "
-			<< ComputeMedianAge(it2, persons.end()) << endl;
+	vector<typename RandomIt::value_type> elements(range_begin, range_end);
+	auto half_it = elements.begin() + range_length / 2;
+	MergeSort(elements.begin(), half_it);
+	MergeSort(half_it, elements.end());
+	merge(elements.begin(), half_it, half_it, elements.end(), range_begin);
 }
 
 int main() {
-	vector<Person> persons = { { 31, Gender::MALE, false }, { 40,
-			Gender::FEMALE, true }, { 24, Gender::MALE, true }, { 20,
-			Gender::FEMALE, true }, { 80, Gender::FEMALE, false }, { 78,
-			Gender::MALE, false }, { 10, Gender::FEMALE, false }, { 55,
-			Gender::MALE, true }, };
-	PrintStats(persons);
+	vector<int> v = { 6, 4, 7, 6, 4, 4, 0, 1 };
+	MergeSort(begin(v), end(v));
+	for (int x : v) {
+		cout << x << " ";
+	}
+	cout << endl;
 	return 0;
 }
