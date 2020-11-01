010f7f7dbd42e973e17a3bec30bea78dc1f706fd
Dipod <dipod.dopid@gmail.com>
2020-11-01 15:03:17 +0300

    Task "Inheritance by own hands" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,29 @@
 #include <iostream>
-#include <string>
-#include <deque>
 
 using namespace std;
 
+class Animal {
+public:
+	Animal(const string &name) :
+			Name(name) {
+	}
+
+	const string Name;
+};
+
+class Dog: public Animal {
+public:
+	Dog(const string &name) :
+			Animal(name) {
+	}
+
+	void Bark() {
+		cout << Name << " barks: woof!" << endl;
+	}
+};
+
 int main() {
-
-	deque<string> result(1);
-	int n = 0;
-	cin >> result[0];
-
-	cin >> n;
-	char operation, prev_operation;
-	int operand = 0;
-	bool first_operation = true;
-	for (int i = 0; i < n; i++) {
-		cin >> operation;
-		cin >> operand;
-		if (first_operation) {
-			string tail = " ";
-			tail.append(1, operation);
-			result.push_back(tail + " " + to_string(operand));
-			first_operation = false;
-		} else {
-			if ((prev_operation == '-' || prev_operation == '+')
-					&& (operation == '*' || operation == '/')) {
-				result.push_front("(");
-				string tail = ") ";
-				tail.append(1, operation);
-				result.push_back(tail + " " + to_string(operand));
-			} else {
-				string tail = " ";
-				tail.append(1, operation);
-				result.push_back(tail + " " + to_string(operand));
-			}
-		}
-		prev_operation = operation;
-	}
-
-	for (const auto &string_part : result) {
-		cout << string_part;
-	}
-
+	Dog d("name");
+	d.Bark();
 	return 0;
 }
