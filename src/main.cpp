6b88d1b86470e1b1d2f7937edb918556422c90d5
Dipod <dipod.dopid@gmail.com>
2020-11-04 20:54:09 +0300

    Task "Sending notifications" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,147 +1,56 @@
 #include <iostream>
-#include <memory>
-#include <stack>
-#include <vector>
 #include <string>
-#include <cstdint>
 
 using namespace std;
 
-// Внимание!
-// Для простоты разбора будем использовать только числа из одной цифры,
-// а также не будет скобок, пробелов и ненужных символов.
-// При этом, будем считать, что выражение всегда корректно.
+void SendSms(const string &number, const string &message) {
+	cout << "Send '" << message << "' to number " << number << endl;
+}
 
-struct Node {
-	virtual int Evaluate() const = 0;
+void SendEmail(const string &email, const string &message) {
+	cout << "Send '" << message << "' to e-mail " << email << endl;
+}
+
+class INotifier {
+public:
+	virtual ~INotifier() {
+	}
+	virtual void Notify(const string &message) const = 0;
 };
 
-struct Value: public Node {
-	Value(char digit) :
-			_value(digit - '0') {
+class SmsNotifier: public INotifier {
+public:
+	SmsNotifier(const string &phone_number) :
+			phone_number_(phone_number) {
 	}
-
-	int Evaluate() const override {
-		return _value;
+	void Notify(const string &message) const override {
+		SendSms(phone_number_, message);
 	}
-
 private:
-	const uint8_t _value;
+	const string phone_number_;
 };
 
-struct Variable: public Node {
-	Variable(const int &x) :
-			_x(x) {
+class EmailNotifier: public INotifier {
+public:
+	EmailNotifier(const string &email_adress) :
+			email_adress_(email_adress) {
 	}
-
-	int Evaluate() const override {
-		return _x;
+	void Notify(const string &message) const override {
+		SendEmail(email_adress_, message);
 	}
-
 private:
-	const int &_x;
+	const string email_adress_;
 };
 
-struct Op: public Node {
-	Op(char value) :
-			precedence([value] {
-				if (value == '*') {
-					return 2;
-				} else {
-					return 1;
-				}
-			}()), _op(value) {
-	}
-
-	const uint8_t precedence;
-
-	int Evaluate() const override {
-		if (_op == '*') {
-			return _left->Evaluate() * _right->Evaluate();
-		} else if (_op == '+') {
-			return _left->Evaluate() + _right->Evaluate();
-		} else if (_op == '-') {
-			return _left->Evaluate() - _right->Evaluate();
-		}
-
-		return 0;
-	}
-
-	void SetLeft(shared_ptr<Node> node) {
-		_left = node;
-	}
-	void SetRight(shared_ptr<Node> node) {
-		_right = node;
-	}
-
-private:
-	const char _op;
-	shared_ptr<const Node> _left, _right;
-};
-
-template<class Iterator>
-shared_ptr<Node> Parse(Iterator token, Iterator end, const int &x) {
-	// Empty expression
-	if (token == end) {
-		return make_shared<Value>('0');
-	}
-
-	stack<shared_ptr<Node>> values;
-	stack<shared_ptr<Op>> ops;
-
-	auto PopOps = [&](int precedence) {
-		while (!ops.empty() && ops.top()->precedence >= precedence) {
-			auto value1 = values.top();
-			values.pop();
-			auto value2 = values.top();
-			values.pop();
-			auto op = ops.top();
-			ops.pop();
-
-			op->SetRight(value1);
-			op->SetLeft(value2);
-
-			values.push(op);
-		}
-	};
-
-	while (token != end) {
-		const auto &value = *token;
-		if (value >= '0' && value <= '9') {
-			values.push(make_shared<Value>(value));
-		} else if (value == 'x') {
-			values.push(make_shared<Variable>(x));
-		} else if (value == '*') {
-			PopOps(2);
-			ops.push(make_shared<Op>(value));
-		} else if (value == '+' || value == '-') {
-			PopOps(1);
-			ops.push(make_shared<Op>(value));
-		}
-
-		++token;
-	}
-
-	while (!ops.empty()) {
-		PopOps(0);
-	}
-
-	return values.top();
+void Notify(INotifier &notifier, const string &message) {
+	notifier.Notify(message);
 }
 
 int main() {
-	string tokens;
-	cout << "Enter expression: ";
-	getline(cin, tokens);
+	SmsNotifier sms { "+7-495-777-77-77" };
+	EmailNotifier email { "na-derevnyu@dedushke.ru" };
 
-	int x = 0;
-	auto node = Parse(tokens.begin(), tokens.end(), x);
-
-	cout << "Enter x: ";
-	while (cin >> x) {
-		cout << "Expression value: " << node->Evaluate() << endl;
-		cout << "Enter x: ";
-	}
-
+	Notify(sms, "I have White belt in C++");
+	Notify(email, "And want a Yellow one");
 	return 0;
 }
